3b56502bd281623721e4b62faa6ff6deba13b1a0
Ethan Kaplan <ethankap@murmurs.com>
2011-04-06 13:58:36 -0700

    finished a bunch of stuff

--- scripts/gh-desc.js
+++ scripts/gh-desc.js
@@ -1,24 +1,50 @@
 var github_url = 'https://github.com/api/v2/json/repos/show';
+var to_add = [];
 
+
+function addTitle (element) {
+	var $this = element;
+	var repo = $this.attr('pathname');
+	if (!$this.attr('title')) {
+		$.ajax({
+			url: github_url + repo,
+			success: function(data) {
+				console.log(data['repository']['description']);
+				$this.attr('title',data['repository']['description']);
+			}
+		});
+	}
+}
+
+function checkTitles() {
+	console.log('executed');
+	$('li.public.source a').each(
+		function() {
+			if (!$(this).attr('title')) {
+				console.log($(this).attr('pathname'));
+				to_add.push($(this));
+			}
+		}
+	);
+	for (var i=0, len=to_add.length; i<len; ++i) {
+		addTitle(to_add[i]);
+	}
+}
 
 
 
 $(document).ready(function() {
-	$('li.public.source a').bind({
-		mouseover : function(element) {
-			var $this = $(this);
-			var repo = $this.attr('pathname');
-			$.ajax({
-				url: github_url + repo,
-				success: function(data) {
-					console.log(data['repository']['description']);
-					$this.attr('title',data['repository']['description']);
-				}
-				
-			})
+	
+	$('li.public.source a').each(
+		function() {
+			addTitle($(this));
 		}
+	);
 
+	$('a.#inline_watched_repos').bind('click',function() {
+		setTimeout('checkTitles()', 1500);
 	});
+
 
 	console.log('started');
 	
